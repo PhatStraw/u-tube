3a69d95e752af41f6ec3b4c5987d1eccda0d6751
kevinsims1 <kevinsims1@outlook.com>
2019-12-02 13:56:25 -0600

    Updates

---
+++ precache-manifest.a6cffec8e8cdbd9c357bf862673b22e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a609c6dd8a4f1d364ecf36710e4f9e4",
+    "url": "/u-tube/index.html"
+  },
+  {
+    "revision": "628e1f2f3b78e38a7f1d",
+    "url": "/u-tube/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "f0683c041dd02102fedd",
+    "url": "/u-tube/static/js/2.2aaf51b3.chunk.js"
+  },
+  {
+    "revision": "628e1f2f3b78e38a7f1d",
+    "url": "/u-tube/static/js/main.1b7b10ba.chunk.js"
+  },
+  {
+    "revision": "bf9effc118104e029e8e",
+    "url": "/u-tube/static/js/runtime-main.99f26de7.js"
+  }
+]);
